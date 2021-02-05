e772243ebc2e6190cc9bf67207996b38ac0d8237
lnoueryo <popo62520908@gmail.com>
2021-02-06 00:38:08 +0900

    chapter49

---
+++ class.ts
@@ -0,0 +1,19 @@
+class Person {
+    name: string;
+    constructor(initName: string){
+        this.name =  initName;
+    }
+
+    greeting(this:{name: string}){
+        console.log(`Hello! My name is ${this.name}`)
+    }
+}
+
+const quill = new Person('quill');
+quill.greeting()
+
+const anotherQuill = {
+    name: 'ore',
+    anotherGreeting: quill.greeting
+}
+anotherQuill.anotherGreeting();
