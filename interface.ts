dbfe3b8b01e34d3448e573c0813c7a1313a13f3f
lnoueryo <popo62520908@gmail.com>
2021-02-06 20:27:09 +0900

    chapter65

--- interface.ts
+++ interface.ts
@@ -4,10 +4,19 @@ interface Human {
     greeting(message: string): void;
 }
 class Developer implements Human{
-    constructor(public name: string, public age: number){
+    constructor(public name: string, public age: number, public experience: number){
 
     }
     greeting(message: string){
         console.log('Hello!');
     }
 }
+const tmpDeveloper = {
+    name: 'Quill',
+    age: 38,
+    experience: 3,
+    greeting(message: string){
+        console.log('Hello!');
+    }
+}
+const user = new Developer('Quill', 38, 3);
