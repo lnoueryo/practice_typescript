f259bea23ad78ce7fd2bb4351b6ab29037de2966
lnoueryo <popo62520908@gmail.com>
2021-03-12 01:52:44 +0900

    type guard

--- advanced.ts
+++ advanced.ts
@@ -1,9 +1,9 @@
-type Engineer = {
+interface Engineer {
     name: string;
     role: string;
 }
 
-type Blogger = {
+interface Blogger {
     name: string;
     follower: number;
 }
@@ -17,3 +17,46 @@ const quill: EngineerBlogger = {
     role: 'front-end',
     follower: 1000
 }
+
+function toUpperCase(x: string | number){
+    if(typeof x === 'string'){
+        x.toUpperCase();
+    }
+    return '';
+}
+
+type NomadWorker = Engineer | Blogger;
+function describeProfile(nomadWorker: NomadWorker){
+    console.log(nomadWorker.name)
+    if('role' in nomadWorker){
+        console.log(nomadWorker.name)
+    }
+    if('follower' in nomadWorker){
+        console.log(nomadWorker.follower)
+    }
+}
+
+class Dog {
+    speak(){
+        console.log('bow-bow')
+    }
+}
+
+class Bird {
+    speak(){
+        console.log('tweet-tweet')
+    }
+    fly(){
+        console.log('flutter')
+    }
+}
+
+type Pet = Dog | Bird;
+function havePet(pet: Pet){
+    pet.speak();
+    if(pet instanceof Bird){
+        pet.fly();
+    }
+}
+
+havePet(new Bird())
